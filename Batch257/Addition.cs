bdaaa7a4e096d31f1c129fad2f196ef1ec7228e3
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-08 08:20:06 +0530

    Addition program

---
+++ Batch257/Addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch257
+{
+    internal class Addition
+    {
+        public static void add()
+        {
+            Console.WriteLine("Addition program");
+            Console.WriteLine("Enter the first number");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int val3 = val1 + val2;
+            Console.WriteLine("Addition will be:" + val3);
+        }
+    }
+}
--- Batch257/Program.cs
+++ Batch257/Program.cs
@@ -9,7 +9,7 @@ namespace Batch257
         {
             Console.WriteLine("Choose your option from below");
             Console.WriteLine("1.Username display\n2.LeapYr\n3.FlipCoin Percentage\n4.Power of number\n5.HarmonicNumber" +
-                "\n6.PrimeFactor");
+                "\n6.PrimeFactor\n7.Addition");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -31,16 +31,10 @@ namespace Batch257
                 case 6:
                     PrimeFactor.Num();
                     break;
+                case 7:
+                    Addition.add();
+                    break;
             }
-
-
-            //Console.WriteLine("Addition program");
-            //Console.WriteLine("Enter the first number");
-            //int val1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter the second number");
-            //int val2 = Convert.ToInt32(Console.ReadLine());
-            //int val3 = val1 + val2;
-            //Console.WriteLine("Addition will be:" + val3);
         }
     }
 }
